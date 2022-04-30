569c18ba00439c394a4e2b2d1a722eebc5733762
Driscoll <jdriscoll1@ewu.edu>
2022-04-30 14:44:46 -0700

    output movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,30 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        float ud = Input.GetAxisRaw("Vertical");
+        float lr = Input.GetAxis("Horizontal");
+
+    
+       
+        if (up != 0 || down != 0)
+        {
+            Debug.Log("Moving");
+
+        }
+        else {
+            Debug.Log("Stationary");
+        }
+        //Key is Down
+        //On Any Key Release
+        //Key is Released
+
     }
 }
