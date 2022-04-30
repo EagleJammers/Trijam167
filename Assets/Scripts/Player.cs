40345ee74d800f752c15d2b1b13eb8ef2451a2db
miklast <mdieatrick@ewu.edu>
2022-04-30 14:55:41 -0700

    Added vertical (W/S) movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public CharacterController controllerMain;
+
+    public float movementSpeed = 2;
+
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        var movement = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0,movement, 0) * Time.deltaTime * movementSpeed;
         
     }
 }
