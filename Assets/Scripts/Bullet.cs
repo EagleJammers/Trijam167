52907249be95c46bdfa7cb8802a0493834913cd3
Clockknight <34818025+Clockknight@users.noreply.github.com>
2022-04-30 16:52:47 -0700

    Added bullet self destruct on collide with player or wall

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float velocity = .01f; 
-    
+    public float velocity = .01f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector3(velocity, 0, 0));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject collide = collision.gameObject;
+        string collidetag = collide.tag;
+
+        if(collidetag == "Player")
+        {
+          //collision.gameObject.damage()
+          Destroy(this);
+        }
+        else if(collidetag == "Wall")
+        {
+          Destroy(this);
+        }
+
     }
 }
