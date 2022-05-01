701425f8809efeda451cd0ccf83ae3d6665b53be
Driscoll <jdriscoll1@ewu.edu>
2022-04-30 17:03:53 -0700

    Back up to date

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject player; 
+    public float velocity = .001f;
+    private float ySpeed;
+    private float xSpeed; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the player's y 
+        float playerY = player.transform.position.y;
+
+        //get my y 
+        float thisY = this.transform.position.y;
+
+        float deltaX = player.transform.position.x - this.transform.position.x;
+        float deltaY = playerY - thisY;
+
+        //Determine the y val 
+        ySpeed = deltaY * velocity;
+        xSpeed = deltaX * velocity; 
+        Debug.Log(ySpeed); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move at this rate x and y 
+        transform.Translate(new Vector3(xSpeed, ySpeed, 0));
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bullet;
+    public int freq = 600;
+
+    private int time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spawns a bullet at its location every 10 seconds
+        time++;
+
+        if (time % freq == 0) {
+
+            Instantiate(bullet, this.transform.position, Quaternion.identity); 
+        }
+    }
+}
